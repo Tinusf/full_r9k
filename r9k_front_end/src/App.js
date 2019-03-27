2a2aa095fd9ddf8db038714a74ea483586b27210
Tinus Flagstad <tinus@flagstad.as>
2019-03-27 22:24:23 +0100

    Add textareas and onSubmits

--- r9k_front_end/src/App.js
+++ r9k_front_end/src/App.js
@@ -5,8 +5,22 @@ import './App.css';
 class App extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      text: ""
+    };
   }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const data = { sentence: this.state.text };
+    fetchData(
+      "http://localhost:5000/insert_sentence",
+      "PUT",
+      data
+    ).then((response) => {
+      console.log(response)
+    });
+  };
 
   fetchAllUsedWords = async () => {
     const used_words = await fetchData(
@@ -33,6 +47,27 @@ class App extends Component {
             <p>{this.state.used_words}</p>
           </div>
         }
+        <div>
+          <textarea
+            className="outputTextarea"
+            readOnly
+            rows="10"
+            value="Welcome to r9k-chat.">
+          </textarea>
+        </div>
+        <form>
+          <textarea
+            className="inputTextarea"
+            rows="2"
+            value={this.state.text}
+            onChange={e => this.setState({ text: e.target.value })}>
+
+          </textarea>
+          <div>
+            <input type="submit" onClick={e => this.handleSubmit(e)} ></input>
+          </div>
+        </form>
+
       </div>
     );
   }
