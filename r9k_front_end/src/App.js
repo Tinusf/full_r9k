9eff24b389cd9a0ce4a6da0f93b0aac8d2d3c66b
Tinus Flagstad <tinus@flagstad.as>
2019-03-27 22:07:54 +0100

    Add react code in order to fetch all used_words and enable CORS etc.

--- r9k_front_end/src/App.js
+++ r9k_front_end/src/App.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { fetchData } from './util/Utils';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  fetchAllUsedWords = async () => {
+    const used_words = await fetchData(
+      "http://localhost:5000/used_words",
+      'GET'
+    ).then((data) => {
+      const text = data.text
+      this.setState({ used_words: text })
+      console.log(data);
+    });
+  };
+
+  async componentDidMount() {
+    this.fetchAllUsedWords();
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.used_words &&
+          <div>
+            <p>used_words:</p>
+            <p>{this.state.used_words}</p>
+          </div>
+        }
       </div>
     );
   }
--- server.py
+++ server.py
@@ -1,26 +1,33 @@
 from flask import Flask, request
+from flask_cors import CORS
 import json
 
 app = Flask(__name__)
+CORS(app)
 
 used_words = set()
 
 
+def format_answer(text, status_code=200):
+    # TODO: fix status_code.
+    return json.dumps({"status": status_code, "text": text})
+
+
 @app.route("/")
 def hello():
-    return "Hello"
+    return format_answer("Hello")
 
 
 @app.route("/insert_sentence", methods=["PUT"])
 def insert_word():
     json_input_data = json.loads(request.data)
     sentence = json_input_data["sentence"]
-    return check_unique(sentence)
+    return format_answer(check_unique(sentence))
 
 
 @app.route("/used_words", methods=["GET"])
 def get_all_words():
-    return ", ".join(used_words)
+    return format_answer(", ".join(used_words))
 
 
 def check_unique(sentence):
